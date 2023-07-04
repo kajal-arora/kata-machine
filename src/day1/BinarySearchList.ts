8dd018269730d170751625afa8dc1d0744c95b76
Kajal Arora <a.kajal1405@gmail.com>
2023-07-04 16:14:46 +0530

    Implemented binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,42 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    // with recursion
+    /*   if (haystack.length === 0) {
+        return false;
+    }
+    let middle = Math.floor(haystack.length / 2);
+    if (haystack[middle] === needle) {
+        return true;
+    }
+    else if (haystack[middle] < needle) {
+        let left = middle + 1;
+        let newArr = haystack.slice(left);
+        return bs_list(newArr, needle);
+    } else {
+        let right = middle;
+        let newArr = haystack.slice(0, right);
+        return bs_list(newArr, needle);
+    }
+*/
 
+    //without recursion
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        let middle = Math.floor(low + (high - low)/2);
+        let midValue = haystack[middle];
+
+        if(needle === midValue) {
+            return true;
+        }
+        else if(needle > midValue) {
+            low = middle + 1;
+        }
+        else {
+            high = middle;
+        }
+
+    } while (low < high);
+
+    return false;
 }
