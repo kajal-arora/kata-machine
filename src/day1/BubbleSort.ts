a6213751e972a4f8500bf899125356dc68eae028
Kajal Arora <a.kajal1405@gmail.com>
2023-07-10 01:52:55 +0530

    Added bubble sort and two crystal problem

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,28 @@
 export default function bubble_sort(arr: number[]): void {
+    //Complexity -  O(N^2)
+    // for(let i = 0 ;i<arr.length; i++) {
+    //     for(let j = 0; j<arr.length; j++) {
+    //         if(arr[j] > arr[j+1]) {
+    //             let temp = arr[j];
+    //             arr[j] = arr[j+1];
+    //             arr[j+1] = temp;
+    //         }
+    //     }
+    // }
 
+    /*
+    arr.length-1-i 
+    - 1 = to not include the last element in the first iteration so minus 1 
+    - i = every single time we execute, the last element becomes the largest, so need to redo that so minus i
+    */
+
+    for(let i = 0 ;i<arr.length; i++) {
+        for(let j = 0; j<arr.length-1-i; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,30 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    /*
+    Generalised version - 
+        When given two crystal balls that will break if dropped from a high 
+        enough distance, determine the exact spot in which it will break in the 
+        most optimized way.
 
+    Interview Ques - 
+        You are in 100 storey building and you have 2 crsytal balls. 
+        Find out which floor do they break.     
+
+    Solution - Had it been implemented in binary search by dividing in half for first ball, I might miss out on one ball and then had to do linear search since I'hv only two balls.  
+    */
+    let jumpCount = Math.floor(Math.sqrt(breaks.length)); 
+    let i = jumpCount;
+    for(; i<breaks.length ; i+=jumpCount) {
+        if(breaks[i]) {
+            break;
+        }
+    }
+
+    i -=jumpCount;
+
+    for(let j=0; j<jumpCount && i<breaks.length  ; j++, i++) {
+        if(breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
